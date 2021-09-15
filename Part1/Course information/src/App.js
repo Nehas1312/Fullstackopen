cd456bb61229ddfde7a20f49504f81a89c10e474
Nehas1312 <nehaskandiraju@gmail.com>
2021-09-15 11:18:28 +0530

    part1 CI  step 5

--- Part1/Course information/src/App.js
+++ Part1/Course information/src/App.js
@@ -1,29 +1,32 @@
 import React from 'react'
 
 const App = () => {
-  const course = 'Half Stack application development'
-  const parts = [
-    {
-      name: 'Fundamentals of React',
-      exercises: 10
-    },
-    {
-      name: 'Using props to pass data',
-      exercises: 7
-    },
-    {
-      name: 'State of a component',
-      exercises: 14
-    }
-  ]
-  const val = parts.map(part => <p> {part.name} - {part.exercises} </p>  )
-  console.log(val)
-  return (
+  
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+    
+  }
+const val = course.parts.map(value => <p> {value.name} - {value.exercises} </p> );
+console.log(val)
+     return (
     <div>
-      <h1> {course}</h1>
-       {val} 
-      
+     {course.name}
+     <p> {val} </p>
     </div>
   )
 }
-export default App
+export default App;

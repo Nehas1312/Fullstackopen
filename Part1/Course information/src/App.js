911b06f38d17f9712bd5b5aea454469b7b4acf5c
Nehas1312 <nehaskandiraju@gmail.com>
2021-09-23 15:54:07 +0530

    Unicafe Step 2

--- Part1/Course information/src/App.js
+++ Part1/Course information/src/App.js
@@ -1,32 +1,41 @@
-import React from 'react'
+import React, { useState } from 'react'
+
+
 
 const App = () => {
+  // save clicks of each button to its own state
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+  const [all,setAll] = useState(good + bad +neutral)
+  const clickgood = () => {
+    setAll(all+1)
+    setGood(good + 1)}
+    const clicknetural = () => {
+      setAll(all+1)
+      setNeutral(neutral + 1)}
+      const clickbad = () => {
+        setAll(all+1)
+        setBad(bad + 1)}  
   
-  const course = {
-    name: 'Half Stack application development',
-    parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7
-      },
-      {
-        name: 'State of a component',
-        exercises: 14
-      }
-    ]
-    
-  }
-const val = course.parts.map(value => <p> {value.name} - {value.exercises} </p> );
-console.log(val)
-     return (
+  return (
     <div>
-     {course.name}
-     <p> {val} </p>
+      <h1> Give feedback</h1>
+      <button onClick={clickgood} > Good</button>
+      <button onClick={clicknetural}>Netural</button>
+      <button onClick={clickbad}>Bad</button>
+      <h2>
+        Statistics
+      </h2>
+      <li>Good= {good}</li> 
+      <li>Netural={neutral} </li>
+      <li>Bad= {bad}</li>  
+      <li>All = {all}</li>
+      <li>Average= {((good-bad)/all)  }</li>
+      <li>Positve feedback={((good)/all)*100 }%</li>
+
     </div>
   )
 }
-export default App;
+
+export default App

029388f4bcf93004d8b02f456fb0b9492a908e16
Nehas1312 <nehaskandiraju@gmail.com>
2021-09-29 18:41:12 +0530

    Anecdotes step 2 and step 3

--- Part1/Anecdotes/src/App.js
+++ Part1/Anecdotes/src/App.js
@@ -10,12 +10,47 @@ const App = () => {
     'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
     'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients'
   ]
+  
+   const [vote,setVote] = useState(new Uint8Array(7))
+  
+
+  const copy = [...vote]
+   function indexOfMax(arr) {
+    if (arr.length === 0) {
+        return -1;
+    }
+
+    var max = arr[0];
+    var maxIndex = 0;
+
+    for (var i = 1; i < arr.length; i++) {
+        if (arr[i] > max) {
+            maxIndex = i;
+            max = arr[i];
+        }
+    }
+
+    return maxIndex;
+}
    
-  const [selected, setSelected] = useState(0)
+
+ const [selected, setSelected] = useState(0)
+ function getRandomInt(max) {
+  return Math.floor(Math.random() * max)}
 
   return (
     <div>
-      {anecdotes[selected]}
+      <h1> Anecdote of the day</h1>  
+     <td> {anecdotes[selected]} </td>
+     <td1> Has {vote[selected]}  votes.</td1>
+    
+      <p><button onClick ={ () => {copy[selected]+=1;
+      setVote(copy)}} > Vote</button>
+        <button onClick ={ () => setSelected(getRandomInt(7))}> Next anecdotes</button>
+       </p>
+       <h2> Anecdote with most votes</h2>
+        <p> {anecdotes[indexOfMax(vote)]}</p>
+        <p1> has {vote[indexOfMax(vote)]} votes.</p1>
     </div>
   )
 }

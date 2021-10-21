a41beed2d8286f9cd3e8969cdd485a17c06ac9d0
Nehas1312 <nehaskandiraju@gmail.com>
2021-10-21 15:46:29 +0530

    Course information step 8 and Step 9

--- Part2/course information/src/App.js
+++ Part2/course information/src/App.js
@@ -1,46 +1,78 @@
 import React, { useState } from 'react'
 const App = () => {
-  const course = {
-    id: 1,
-    name: 'Half Stack application development', 
-    parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10,
-        id: 1
-      },
-      {
-        name: 'Using props to pass data', 
-        exercises: 7,
-        id: 2
-      },
-      {
-        name: 'State of a component', 
-        exercises: 14,
-        id: 3
-      }
-    ]
-  }
+  const courses = [
+    {
+      name: 'Half Stack application development',
+      id: 1,
+      parts: [
+        {
+          name: 'Fundamentals of React',
+          exercises: 10,
+          id: 1
+        },
+        {
+          name: 'Using props to pass data',
+          exercises: 7,
+          id: 2
+        },
+        {
+          name: 'State of a component',
+          exercises: 14,
+          id: 3
+        },
+        {
+          name: 'Redux',
+          exercises: 11,
+          id: 4
+        }
+      ]
+    }, 
+    {
+      name: 'Node.js',
+      id: 2,
+      parts: [
+        {
+          name: 'Routing',
+          exercises: 3,
+          id: 1
+        },
+        {
+          name: 'Middlewares',
+          exercises: 7,
+          id: 2
+        }
+      ]
+    }
+  ]
   const Course =(props)=>{
-  //  var all =0
-  //  for (var i= 0;i<props.course.parts.length;i++){
-  //    all+= props.course.parts[i].exercises 
+  //  for (var i= 0;i<props.course.length;i++){
+  //    for (var j = 0; j< props.course[i].parts.length;j++){
+  //      return(
+  //                <div> <h1>{props.course[i].name} </h1>
+  //                   <p> {props.course.parts[j].name} - {props.course.parts[j].exercises} </p> </div>
+      
+  //      )
+  //    }
   //  }
 
     return( 
   <div>
-    { <h1> {props.course.name} </h1>}
-    {props.course.parts.map(value => 
-        <p key={value.id}> {value.name} - {value.exercises} </p>
+  <h1> Web development curriculum </h1> 
+    {props.course.map(val => <p1  > <h2 key = {val.id}> {val.name} </h2>
+      {val.parts.map(value => <p
+         key={value.id}> {value.name} - {value.exercises} 
+            </p>
            )}
-  {/* <p> Total excercises - {all}</p> */}
-   <p> Total excercises - {props.course.parts.reduce((sum,excercise) =>  sum +excercise.exercises,0)}</p>
+      { <p> Total excercises - {val.parts.reduce((sum,excercise) =>  sum +excercise.exercises,0)}</p> 
+} 
+    </p1>)}
+
 
 
 
     </div>)}  
 
-  return <Course course={course} />
+  return <Course course={courses} />
 }
 
 export default App

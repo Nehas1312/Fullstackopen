92cf4251194d06e76ad1945f6b88ada1d3594c0f
Nehas1312 <nehaskandiraju@gmail.com>
2022-01-13 20:58:14 +0530

    phonebook backend step 4,5,6

--- Part3/Phonebook backend/index.js
+++ Part3/Phonebook backend/index.js
@@ -1,6 +1,7 @@
 const { request, response } = require("express");
 const express = require("express");
 const app = express();
+app.use(express.json())
 let persons = [
   {
     id: 1,
@@ -24,6 +25,13 @@ let persons = [
   },
 ];
 
+const generateId = () => {
+  const maxId = persons.length > 0
+    ? Math.max(...persons.map(n => n.id))
+    : 0
+  return maxId + 1
+}
+
 app.get("/api/persons", (request, response) => {
   response.json(persons);
 });
@@ -46,7 +54,40 @@ app.get('/api/persons/:id',(request,response)=>{
   }
   else{
     response.status(404).end()
+  } 
+})
+
+app.post('/api/persons',(request,response)=>{
+  const body = request.body
+  
+  if (persons.findIndex((person) => person.name === body.name) !== -1) {
+    return response.status(404).json({
+      error : 'name must be unique'
+    })
   }
+  
+   else if (!body.name || !body.number){
+    return  response.status(404).json({
+    error :'name or number is missing'
+  })
+  }
+
+  const person = {
+    name : body.name,
+    number:body.number,
+    date: new Date(),
+    id:generateId(),
+  }
+  
+  persons = persons.concat(person)
+  response.json(person)
+})
+
+app.delete('/api/persons/:id',(request,response)=>{
+  const id = Number(request.params.id)
+  const person = persons.filter(person => person.id !== id)
+  response.status(204).end()
+  console.log(`Deleted sucessfully`)
 })
 
 const PORT = 3001;
